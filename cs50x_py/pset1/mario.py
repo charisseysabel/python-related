ad569801ca55988304b047a902e7c05fe58ed835
Charisse <charisse.ysabel930@gmail.com>
2015-12-21 17:40:30 +0100

    Add python version of CS50x C problem set: 1

---
+++ cs50x_py/pset1/mario.py
@@ -0,0 +1,12 @@
+# pset1: mario
+# from CS50x
+# Build a half pyramid according to user's height input
+# python version
+
+height = int(raw_input("> Give me a number between 0 and 23: "))
+if height < 0 or height > 23:
+	print "Invalid input."
+
+for row in range(1, height + 1):
+	print " " * (height - 1) + "#" * row
+	height -= 1
